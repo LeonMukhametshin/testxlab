8c08c528bfe4bf45db63f9f543c19bcc400edc03
Leon <leonmukhametshin2004@gmail.com>
2025-11-07 18:07:13 +0300

    Create PlayerController and CloudController

---
+++ Assets/Scripts/CloudController.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class CloudController : MonoBehaviour
+{
+    [SerializeField] private Transform[] m_people;
+    [SerializeField] private float m_speed = 2;
+
+    private bool m_isMove;
+    private int m_index = -1;
+    private Vector3 m_position;
+
+    public void MoveNext()
+    {
+        m_index++;
+
+        if (m_index >= m_people.Length)
+        {
+            m_index = 0;
+        }
+
+        m_position = m_people[m_index].position;
+        m_position.y = transform.position.y;
+
+        m_isMove = true;
+    }
+
+    private void Update()
+    {
+        if (!m_isMove) return;
+
+        transform.position = Vector3.Lerp(transform.position, m_position, m_speed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, m_position) < 0.1f)
+        {
+            m_isMove = false;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        Debug.Log("Удален");
+    }
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private FreeCamera m_freeCamera;
+    [SerializeField] private GameObject m_uiPanel;
+    [SerializeField] private CloudController m_cloudController;
+
+    private void Update()
+    {
+        if (m_uiPanel.activeSelf) return;
+        m_freeCamera.Move();
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            m_cloudController.MoveNext();
+        }
+    }
+}
